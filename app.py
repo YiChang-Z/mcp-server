981f13d4a6a8a5d85a3c0ebeba724f5c3260f134
okey <okey@cht.com.tw>
2025-12-29 13:44:13 +0800

    edit app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = FastMCP("My MCP Server")
 
 # 2. 提供一個私有的服務或資料給 ChatGPT/Claude
 @app.tool()
-def get_private_core_value() -> str:
+def get_private_core_value(**kwargs) -> str:
     """Provide private setting value of user okey"""
     return "ey_JjawQi0iI0Y2M3MzM1NC02MDU0LTRkNmItODJlMVMzI0MDQ1ZmEilCjJc"
 
 # 3. 兩數相加的工具
 @app.tool()
-def add_numbers(a: int, b: int) -> int:
+def add_numbers(a: int, b: int,**kwargs) -> int:
     """Add Two Numbers"""
     return a + b
 
