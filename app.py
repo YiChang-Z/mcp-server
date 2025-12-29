67f36a596f6d798f8fe7175fd77d543c00cd6dde
okey <okey@cht.com.tw>
2025-12-29 13:52:46 +0800

    edit ...Optional... app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastmcp import FastMCP
 
 # 1. 建立 FastMCP 實例
@@ -5,13 +6,13 @@ app = FastMCP("My MCP Server")
 
 # 2. 提供一個私有的服務或資料給 ChatGPT/Claude
 @app.tool()
-def get_private_core_value(**kwargs) -> str:
+def get_private_core_value(task_progress: Optional[any] = None) -> str:
     """Provide private setting value of user okey"""
     return "ey_JjawQi0iI0Y2M3MzM1NC02MDU0LTRkNmItODJlMVMzI0MDQ1ZmEilCjJc"
 
 # 3. 兩數相加的工具
 @app.tool()
-def add_numbers(a: int, b: int,**kwargs) -> int:
+def add_numbers(a: int, b: int, task_progress: Optional[any] = None) -> int:
     """Add Two Numbers"""
     return a + b
 
